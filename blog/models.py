fcb4dd8ee4031b06c58c6de21012ba65de8b529b
Your Name <o.a.degtyareva@gmail.com>
2016-03-05 17:30:13 +0300

    была миграция

--- blog/models.py
+++ blog/models.py
@@ -8,15 +8,14 @@ class Post(models.Model):
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
-publisher_date = models.DateTimeField(
-	blank=True, null=True)
+	published_date = models.DateTimeField(blank=True, null=True)
 
-def publish(self):
-	self.publisher_date = timezone.now()
-	self.save()
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
 
-def __str__(self):
-	return self.title
+	def __str__(self):
+		return self.title
 
 
 # Create your models here.
